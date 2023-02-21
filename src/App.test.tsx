c547d98700f7ceea583912c4089e67b6ee9aeb4b
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-02-21 23:32:46 +0700

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it("my test-case1",()=>{
+  render(<><div>My name is ART</div></>)
+  //คำว่า get ต้องเป็น การแสดงผลทันที  คำว่า get มันจะมองหา element ซึ่งจะเจอคำว่า My name is ART
+  // /ART/
+  expect(screen.getByText(/ART/)).toBeInTheDocument();
+})
